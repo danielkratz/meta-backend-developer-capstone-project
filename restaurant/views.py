081db79f890eaed05691d32da966983b24b1cfd1
Daniel Kratz <git@danielkratz.com>
2023-12-22 16:42:10 +0100

    restore index view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+
+def index(request):
+    return render(request, 'index.html', {})
 
 class MenuItemsView(ListCreateAPIView):
     queryset = Menu.objects.all()
